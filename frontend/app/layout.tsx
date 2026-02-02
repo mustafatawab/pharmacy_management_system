a5306872fac5c90be71f3efa5ac38e820b7cbd1e
mustafatawab <engrmustafatawab05@gmail.com>
2026-02-02 15:00:05 +0500

    layout.tsx

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,19 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import { ThemeProvider } from "@/components/theme-provider";
 import "./globals.css";
-import { Providers } from "./Provider";
-import { redirect } from "next/navigation";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata = {
   title: "Pharmacy Management System",
