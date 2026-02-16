91e3c060676771f3fd8042cdc1173d792a0d356e
mustafatawab <engrmustafatawab05@gmail.com>
2026-02-16 12:37:03 +0500

    added https://pharmacy-management-system-beta.vercel.app/ into CORS

--- backend/main.py
+++ backend/main.py
@@ -18,7 +18,7 @@ app.include_router(user_router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "https://pharmacy-management-system-beta.vercel.app"],
     allow_credentials=True,         
     allow_methods=["*"],
     allow_headers=["*"], 
